76300e4f7a67dd0eeb184aa91c598e7b9d76f8d9
Rainier <rainier@meusesoft.com>
2016-02-19 08:22:44 +0100

    .

--- Pebble Time Manager/Pages/JavascriptPage.xaml.cs
+++ Pebble Time Manager/Pages/JavascriptPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,19 +27,38 @@ namespace Pebble_Time_Manager.Pages
         public JavascriptPage()
         {
             this.InitializeComponent();
+
+            LoadText();
         }
+
+        private async void LoadText()
+        {
+            string filepath = @"Assets\javascript.txt";
+            StorageFolder folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            StorageFile file = await folder.GetFileAsync(filepath); // error here
+            var Lines = await Windows.Storage.FileIO.ReadTextAsync(file);
+
+            txtScript.Text = Lines;
+
+
+        }
+
+        private Pebble _Pebble = new Pebble();
+        private Engine _JintEngine;
 
         private void btnExecute_Click(object sender, RoutedEventArgs e)
         {
 
             try
             {
-                var engine = new Engine()
+                _JintEngine = new Engine()
                     .SetValue("log", new Action<object>(Debug))
-                    .SetValue("localStorage", new LocalStorage());
-                    ;
+                    .SetValue("localStorage", new LocalStorage())
+                    .SetValue("console", new Console())
+                    .SetValue("Pebble", _Pebble);
+                ;
 
-                engine.Execute(txtScript.Text);
+                _JintEngine.Execute(txtScript.Text);
             }
             catch (Exception exp)
             {
@@ -51,12 +71,85 @@ namespace Pebble_Time_Manager.Pages
             txtResult.Text = text.ToString();
         }
 
+        public class Console
+        {
+            public void log(String value)
+            {
+                System.Diagnostics.Debug.WriteLine(String.Format("log: {0}", value));
+            }
+        }
+
         private class LocalStorage
         {
             public object getItem(String obj)
             {
                 return "{}";
             }
+        }
+
+        private class Pebble
+        {
+            public void addEventListener(String Event, object function)
+            { 
+                System.Diagnostics.Debug.WriteLine(String.Format("addEventListener(Event = {0}, Function = {1})", Event, function.ToString()));
+
+                EventListeners.Add(Event, function);
+            }
+
+            public void openURL(String URL)
+            {
+                System.Diagnostics.Debug.WriteLine(String.Format("openURL(URL={0})",URL));
+            }
+
+            public void sendAppMessage(object data)
+            {
+                System.Diagnostics.Debug.WriteLine(String.Format("sendAppMessage(data={0})", data.ToString()));
+            }
+
+            private Dictionary<String, object> _EventListeners;
+
+            public Dictionary<String, object> EventListeners
+            {
+                get
+                {
+                    if (_EventListeners == null) _EventListeners = new Dictionary<string, object>();
+                    return _EventListeners;
+                }
+            }
+        }
+
+        private void btnAppMesssage_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var jsfunction = _Pebble.EventListeners["appmessage"];
+
+                System.Func<Jint.Native.JsValue, Jint.Native.JsValue[], Jint.Native.JsValue> _func = jsfunction as System.Func<Jint.Native.JsValue, Jint.Native.JsValue[], Jint.Native.JsValue>;
+
+                AppMessage AM = new AppMessage();
+                AM.payload.request_weather = true;
+
+                Jint.Native.JsValue A = Jint.Native.JsValue.FromObject(_JintEngine, AM);
+                Jint.Native.JsValue[] B = new Jint.Native.JsValue[2];
+                B[0] = new Jint.Native.JsValue(0);
+                B[1] = new Jint.Native.JsValue(1);
+
+                _JintEngine.SetValue("e", AM);
+
+                Jint.Native.JsValue C = _func.Invoke(A, B);
+            }
+            catch (Exception exp)
+            {
+                txtResult.Text = exp.Message;
+            }
+
+        }
+
+        private class AppMessage
+        {
+            public struct A { public bool request_weather; };
+
+            public A payload;
         }
     }
 }
